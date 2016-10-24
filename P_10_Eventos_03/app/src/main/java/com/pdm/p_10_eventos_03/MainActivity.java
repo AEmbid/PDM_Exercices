5d3659b9ae3db176d99e804aa98b29492e70434a
AEmbid <adrian.embid@gmail.com>
2016-10-24 12:35:15 +0200

    Finished P_10

--- P_10_Eventos_03/app/src/main/java/com/pdm/p_10_eventos_03/MainActivity.java
+++ P_10_Eventos_03/app/src/main/java/com/pdm/p_10_eventos_03/MainActivity.java
@@ -2,12 +2,15 @@ package com.pdm.p_10_eventos_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,17 +24,25 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 EditText editText1 = (EditText) findViewById(R.id.editText1);
                 EditText editText2 = (EditText) findViewById(R.id.editText2);
-                RadioButton radioButton=(RadioButton)findViewById(R.id.radioButton);
+
                 TextView textView = (TextView) findViewById(R.id.textView2);
+
                 String valor1 = editText1.getText().toString();
-                double num1 = Double.parseDouble(valor1);
                 String valor2 = editText2.getText().toString();
-                double num2 = Double.parseDouble(valor2);
+
+                double num1;
+                double num2;
+
+                if (TextUtils.isEmpty(valor1)) {num1=0;} else {num1 = Double.parseDouble(valor1);}
+                if (TextUtils.isEmpty(valor2)) {num2=1;} else {num2 = Double.parseDouble(valor2);}
+
                 double operacion = 0;
-                if (radioButton.isChecked())
-                    operacion=num1*num2;
-                if (checkedId==R.id.radioButton2)
-                    operacion=num1/num2;
+
+                if (checkedId==R.id.radioButton) {
+                    operacion = num1 * num2;
+                } else if (checkedId==R.id.radioButton2) {
+                    operacion = num1 / num2;
+                }
                 String resultado = String.valueOf(operacion);
                 textView.setText(resultado);
             }
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ P_06_Material_01 	//TODO
 P_07				//TODO
 P_08_Eventos_01		//FINISHED
 P_09_Eventos_02		//FINISHED
-P_10_Eventos_03		//TODO
+P_10_Eventos_03		//FINISHED
 P_11_Eventos_04		//TODO
 P_12_Eventos_05		//TODO
 P_13_Eventos_06		//TODO
