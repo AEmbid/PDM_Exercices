0ef2d1bc9c5eb794ef46d17797201c7e08944c4b
AEmbid <adrian.embid@gmail.com>
2016-11-03 19:50:42 +0100

    Added P_19 and P_20

---
+++ P_19_Intent_IMC/app/src/main/java/com/pdm/p19_intent_imc/MainActivity.java
@@ -0,0 +1,68 @@
+package com.pdm.p19_intent_imc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btncalc = (Button) findViewById(R.id.btndo);
+        btncalc.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        EditText al = (EditText) findViewById(R.id.editText2);
+        EditText pe = (EditText) findViewById(R.id.editText);
+        switch (v.getId()) {
+            case R.id.btndo:
+                if (TextUtils.isEmpty(al.getText()) ||
+                        TextUtils.isEmpty(pe.getText()) ||
+                        Double.parseDouble(al.getText().toString()) == 0 ||
+                        Double.parseDouble(pe.getText().toString()) == 0 ) {
+
+                    Toast.makeText(this, "Algo no es correcto", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(this, getData(
+                            Integer.parseInt(al.getText().toString()),
+                            Double.parseDouble(pe.getText().toString())
+                    ), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btnhelp:
+                Intent showhelp = new Intent(this, SegundaActivity.class);
+                startActivity(showhelp);
+                break;
+            default:
+                //acciones para resto de botones;
+        }
+    }
+
+    public String getData(int alt, double peso) {
+        String helper = "Tu IMC es ";
+        double al = alt;
+        double n = peso / ((al/100)*(al/100));
+        String h;
+        if (n<18.5) {h="Bajo peso";}
+        else if (n<25) {h="Tienes un peso normal";}
+        else if (n<30) {h="Tienes sobrepeso";}
+        else {h="Eres obeso";}
+
+        return (helper+n+"\n"+h);
+    }
+}
